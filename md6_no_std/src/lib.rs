83b3857c360ee4efd4fa47cb7198d5bbaee9a873
duguorong <80258679+duguorong009@users.noreply.github.com>
2024-03-22 03:22:07 +0800

    fix: increase the input block size as U128(128 bytes)

--- md6_no_std/src/lib.rs
+++ md6_no_std/src/lib.rs
@@ -5,7 +5,7 @@ pub use digest::{self, Digest};
 use core::fmt;
 use digest::{
     block_buffer::Eager,
-    consts::{U16, U28, U32, U48, U64, U8},
+    consts::{U16, U28, U32, U48, U64, U8, U128},
     core_api::{
         AlgorithmName, Block, BlockSizeUser, Buffer, BufferKindUser, CoreWrapper, FixedOutputCore,
         OutputSizeUser, Reset, UpdateCore,
@@ -24,9 +24,9 @@ mod state;
 
 use crate::state::MD6State;
 
-impl_md6!(Md6_64Core, Md6_64, U8, U8, 64);
-impl_md6!(Md6_128Core, Md6_128, U8, U16, 128);
-impl_md6!(Md6_224Core, Md6_224, U8, U28, 224);
-impl_md6!(Md6_256Core, Md6_256, U8, U32, 256);
-impl_md6!(Md6_384Core, Md6_384, U8, U48, 384);
-impl_md6!(Md6_512Core, Md6_512, U8, U64, 512);
+impl_md6!(Md6_64Core, Md6_64, U128, U8, 64);
+impl_md6!(Md6_128Core, Md6_128, U128, U16, 128);
+impl_md6!(Md6_224Core, Md6_224, U128, U28, 224);
+impl_md6!(Md6_256Core, Md6_256, U128, U32, 256);
+impl_md6!(Md6_384Core, Md6_384, U128, U48, 384);
+impl_md6!(Md6_512Core, Md6_512, U128, U64, 512);
